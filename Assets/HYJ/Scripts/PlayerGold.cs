cde8a7615ae6c5199ef68fd53fc3581ba0604281
HamYoungjoo <hamyoungjoo1004@gmail.com>
2023-09-26 19:01:08 +0900

    [Fix]PlayerGold.cs 수정
    void UpdateGoldUI() 부분을  private void LateUpdate()로 변경 ,
    게임 시작시 CurrentGold의 값을 1000으로 임의로 설정.

--- Assets/HYJ/Scripts/PlayerGold.cs
+++ Assets/HYJ/Scripts/PlayerGold.cs
@@ -8,22 +8,22 @@ using TMPro;
 public class PlayerGold : MonoBehaviour
 {
    
-    private int currentGold = 0; // 처음 주어질 골드의 양 
+    public int currentGold = 0; // 처음 주어질 골드의 양 
 
     public TMP_Text goldText;
 
     void Start()
     {
-        UpdateGoldUI();    
+        currentGold = 1000;
     }
 
     public void AddGold(int amount)
     {
         currentGold += amount; // 몬스터가 죽으면 나올 골드를 더해준다
-        UpdateGoldUI();
+        LateUpdate();
     }
 
-    void UpdateGoldUI()
+    private void LateUpdate()
     {
         goldText.text = currentGold + " G";
     }
